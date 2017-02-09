8b301cd261e4838e69d18346fa3435c53f1a116d
swimly <979741120@qq.com>
2017-02-09 18:21:38 +0800

    添加header

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,34 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <app-header :logo="logo" :nav="nav" :user="user"></app-header>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Header from 'views/header'
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      logo: '这是logo',
+      user: 'admin',
+      nav: [{
+        icon: 'icon-password',
+        text: '修改密码',
+        url: '/edit_password'
+      }, {
+        icon: 'icon-logout',
+        text: '退出登录',
+        url: '/logout'
+      }]
+    }
+  },
+  components: {
+    'app-header': Header
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/views/header.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="header">
+    <h1 v-text="logo"></h1>
+    <ul class="row">
+      <li class="col v-m" v-text="user">
+      </li>
+      <li class="col v-m">
+        <router-link v-for="item in nav" :to="item.url">
+          <span class="iconfont" :class="item.icon"></span>
+          <span v-text="item.text"></span>
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'header',
+  props: {
+    logo: {
+      type: String,
+      default: ''
+    },
+    nav: {
+      type: Array,
+      default: []
+    },
+    user: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      msg: 'header'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
